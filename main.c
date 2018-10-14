60afe61a9a85c3c2f6ff6e889bb2ac11e00e5b6f
baimwk <blwkkz@gmail.com>
2018-10-14 20:48:35 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "xtimer.h"
+#include "timex.h"
+
+/* set interval to 1 second */
+#define INTERVAL (1U * US_PER_SEC)
+
+int main(void)
+{
+    xtimer_ticks32_t last_wakeup = xtimer_now();
+
+    while(1) {
+        xtimer_periodic_wakeup(&last_wakeup, INTERVAL);
+        printf("slept until %" PRIu32 "\n", xtimer_usec_from_ticks(xtimer_now()));
+    }
+
+    return 0;
+}
